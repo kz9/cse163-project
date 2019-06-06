570ee3bfe558f211f7465d2a2db4a33cb5fed392
vkap98 <kap98@uw.edu>
2019-06-06 03:40:04 -0700

    map

--- map.py
+++ map.py
@@ -2,31 +2,53 @@ import pandas as pd
 import geopandas
 import matplotlib
 matplotlib.use("TkAgg")
+import matplotlib.pyplot as plt
 
 
-# def plot_total_counts(shape_data, csv_data):
-#     count = csv_data.filter(items=['country_txt'])
-#     count = count.dropna()
-#     count['count'] = 1
-#     count = count.groupby('country_txt')['count'].sum()
+def plot_year(shape, csv):
+    year = csv.filter(items=['imonth'])
+    year = year[year['imonth'] > 0]
+    year = year.dropna()
+    year['count'] = 1
+    year = year.groupby(['imonth'])['count'].sum()
 
-#     merged_data = shape_data.merge(count, right_on='country_txt',
-#                                    left_on='SOVEREIGNT', how='outer')
+    fig, ax = plt.subplots()
+    plot = year.plot(x='imonth', figsize=(10, 5))
+    plt.title(label='Number of Attacks In Each Month')
+    plt.xlabel('Month')
+    plt.ylabel('Number of Total Attacks')
 
-#     merged_data.plot(column='count', legend=True, figsize=(20, 7))
-#     plt.savefig('1.png')
+    a = ['January', 'February', 'March', 'April', 'May', 'June', 'July',
+         'August', 'September', 'October', 'November', 'December']
+    b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    plt.xticks(b, a, rotation='vertical')
+    return plot
 
 
 def plot_counts(shape_data, csv_data, year):
     count = csv_data.filter(items=['country_txt', 'iyear'])
     count = count[count['iyear'] == year]
+
+    fig, ax = plt.subplots(figsize=(20, 6))
+    shape_data.plot(ax=ax, color='#eeeeee')
+
+    count = count.replace(['Czechia', 'East Germany (GDR)', 'Serbia',
+                           'Slovak Republic', 'Bahamas', 'Tanzania',
+                           'United States', 'West Bank and Gaza Strip',
+                           'West Germany (FRG)'],
+                          ['Czech Republic', 'Germany', 'Republic of Serbia',
+                           'Slovakia', 'The Bahamas',
+                           'United Republic of Tanzania',
+                           'United States of America', 'Israel', 'Germany'])
     count = count.dropna()
     count['count'] = 1
+
     count = count.groupby('country_txt')['count'].sum()
+
     merged_data = shape_data.merge(count, right_on='country_txt',
                                    left_on='SOVEREIGNT', how='outer')
-
-    plot = merged_data.plot(column='count', legend=True, figsize=(20, 7))
+    merged_data = merged_data.dropna()
+    plot = merged_data.plot(ax=ax, column='count', legend=True, cmap='YlOrRd')
     return plot
 
 
@@ -36,7 +58,8 @@ def main():
     shape_data = geopandas.read_file(path)
     shape_data = shape_data.filter(items=['SOVEREIGNT', 'geometry'])
     shape_data = shape_data[~(shape_data['SOVEREIGNT'] == 'Antarctica')]
-    plot_counts(shape_data, csv_data, 2000)
+    # a = plot_counts(shape_data, csv_data, 2000)
+    # plt.show()
 
 
 if __name__ == '__main__':
