0b1640468bebc2fadf39cd9a6c75015c0e17d8c7
kz9 <jkz8889@gmail.com>
2019-06-07 02:48:01 -0700

    web version 1.0 finish

--- bokeh_app/scripts/effect.py
+++ bokeh_app/scripts/effect.py
@@ -4,40 +4,39 @@ from bokeh.plotting import figure
 from bokeh.layouts import column, row, WidgetBox
 from bokeh.models import Panel
 from bokeh.models import ColumnDataSource
-from bokeh.models.widgets import RangeSlider
+from bokeh.models.widgets import RangeSlider, Slider
 
 
-def total_tab(df):
-    year = RangeSlider(start=1970, end=2017, value=(1970, 2017), step=1,
+def effect_tab(df):
+    year = Slider(start=1970, end=2017, value=1970, step=1,
                        title="Year Range")
 
-    def make_data(df, start=1970, end=2017):
-        modified = df.groupby('iyear', as_index=False).count()
-        modified = modified[(modified['iyear'] >= start) &
-                            (modified['iyear'] <= end)]
+    def make_data(df, year=1970):
+        modified = df[df['iyear'] == year].groupby(
+                'imonth', as_index=False)['eventid'].count()
         return ColumnDataSource(modified)
 
     def update():
-        new_src = make_data(df, year.value[0], year.value[1])
+        new_src = make_data(df, year.value)
         src.data.update(new_src.data)
 
     year.on_change('value', lambda attr, old, new: update())
     src = make_data(df)
 
     TOOLTIPS = [
-        ("Year", "@iyear"),
-        ("Counts", "@eventid")
+        ('Month', '@imonth'),
+        ('Counts', '@eventid')
     ]
 
     p = figure(plot_height=800, plot_width=800,
-               title='Total attacks over years',
-               x_axis_label='Years',
+               title='Total attacks over years by month',
+               x_axis_label='Month',
                y_axis_label='Attacks',
                tooltips=TOOLTIPS,
                output_backend="webgl")
-    p.line(x='iyear', y='eventid', source=src, line_width=2)
-    p.circle(x='iyear', y='eventid', source=src, size=8, fill_color='white')
+    p.line(x='imonth', y='eventid', source=src, line_width=2, color='red')
+    p.circle(x='imonth', y='eventid', source=src, size=8, fill_color='white')
 
-    tab = Panel(child=row(year, p), title='Total Attacks')
+    tab = Panel(child=row(year, p), title='Year Effect Attacks')
 
     return tab
