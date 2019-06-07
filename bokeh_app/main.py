dc36be2a1101cb60bfc293dfb7cac3243f1e94ad
kz9 <jkz8889@gmail.com>
2019-06-07 02:48:25 -0700

    web version 1.0 finish

--- bokeh_app/main.py
+++ bokeh_app/main.py
@@ -13,8 +13,8 @@ from bokeh.models.widgets import Tabs
 # Each tab is drawn by one script
 from scripts.total import total_tab
 from scripts.type import type_tab
-#from scripts.table import table_tab
-#from scripts.draw_map import map_tab
+from scripts.map import map_tab
+from scripts.effect import effect_tab
 #from scripts.routes import route_tab
 
 # Read data into dataframes
@@ -22,16 +22,18 @@ df = pd.read_pickle(join(dirname(__file__), 'database', 'data.pkl'))
 
 gdf = pd.read_pickle(join(dirname(__file__), 'database', 'geo.pkl'))
 
+shp = pd.read_pickle(join(dirname(__file__), 'database', 'shp.pkl'))
+
 # create tabs
 tab1 = total_tab(df)
 tab2 = type_tab(df)
-#tab3 = map_tab(gdf)
-#tab4 = static_tab(gdf)
+tab3 = map_tab(df, shp)
+tab4 = effect_tab(df)
 #tab5 = bar_tab(gdf)
 
 
 # Put all the tabs into one application
-tabs = Tabs(tabs=[tab1, tab2])
+tabs = Tabs(tabs=[tab1, tab2, tab3, tab4])
 
 # Put the tabs in the current document for display
 curdoc().add_root(tabs)
